bab9d27f26a2aaf990215ca9de50ab309d2ff561
rnonaka <rnonaka@RNONAKA-JP.jp.oracle.com>
2020-04-22 13:45:22 +0900

    add using backend

--- provider.tf
+++ provider.tf
@@ -6,3 +6,10 @@ provider "oci" {
   region = "${var.region}"
   disable_auto_retries = "true"
 }
+
+terraform {
+   backend "http" {
+     address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/miMuMYjUuuBj_C2rUdQwsHCDxTOPq4nBkO5agdZM_G4/n/gc35002/b/terraform-backend/o/"
+     update_method = "PUT"
+   }
+}
